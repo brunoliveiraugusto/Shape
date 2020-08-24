4bf65895823dda65eeb5fc369ccb8fae372aeff3
Bruno Augusto De Souza Oliveira <bruno.desouza@linx.com.br>
2020-08-24 13:23:19 -0300

    Criada classe Circle, propriedade Radius, implementada herança e sobrescrita do método Area.

---
+++ Shape/Shape/Entities/Circle.cs
@@ -0,0 +1,20 @@
+﻿using Shape.Entities.Enums;
+using System;
+
+namespace Shape.Entities
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius, Color color) : base(color)
+        {
+            this.Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * this.Radius * this.Radius;
+        }
+    }
+}
