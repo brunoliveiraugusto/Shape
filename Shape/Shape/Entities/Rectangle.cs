b8b095bfd8e547b7f56d0d7d319644b97380281f
Bruno Augusto De Souza Oliveira <bruno.desouza@linx.com.br>
2020-08-24 13:13:10 -0300

    Criada propriedades, sobrescrita do método Area e implementada herança

--- Shape/Shape/Entities/Rectangle.cs
+++ Shape/Shape/Entities/Rectangle.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Shape.Entities.Enums;
 
 namespace Shape.Entities
 {
-    class Rectangle
+    class Rectangle : Shape
     {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height, Color color) : base(color)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public override double Area()
+        {
+            return this.Width * this.Height;
+        }
     }
 }
